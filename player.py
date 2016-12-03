1e6437846c56823ad2e6f22cf20228999247ea03
97gushan <97gustavh@gmail.com>
2016-12-03 13:21:56 +0100

    collisionwork

--- player.py
+++ player.py
@@ -11,17 +11,31 @@ class Player:
 
         self.image = pygame.image.load("img/tmp.png")
         self.image_rect = self.image.get_rect()
+        self.upper_box = (self.xpos, self.ypos, 50, 2)
+        self.lower_box = (self.xpos, self.ypos+50, 50, 2)
+        self.center_box = (self.xpos- 25, self.ypos + 3, 50, 48)
 
         self.is_grounded = False
 
     def render(self, screen):
         screen.blit(self.image, self.image_rect)
 
+        pygame.draw.rect(screen, (0,255,255),self.upper_box, 2)
+        pygame.draw.rect(screen, (0,255,255),self.lower_box, 2)
+        pygame.draw.rect(screen, (0,255,0),self.center_box, 1)
+
 
     def update(self, dt):
         self.gravity(dt)
         print(self.speed_y)
         self.image_rect.center = (self.xpos,self.ypos)
+
+        self.upper_box = (self.xpos-25, self.ypos-25, 50, 2)
+        self.lower_box = (self.xpos-25, self.ypos+25, 50, 2)
+        self.center_box = (self.xpos- 27, self.ypos -20, 54, 44)
+
+
+
 
     def move_right(self, dt):
         self.xpos += self.speed * (1 + dt)
@@ -41,7 +55,10 @@ class Player:
             self.speed_y = -1.5
 
     def get_rect(self):
-        return self.image_rect
+        return pygame.Rect(self.upper_box), pygame.Rect(self.lower_box), pygame.Rect(self.center_box)
 
     def set_grounded(self, state):
         self.is_grounded = state
+
+    def set_speed(self, speed):
+        self.speed_y = speed
